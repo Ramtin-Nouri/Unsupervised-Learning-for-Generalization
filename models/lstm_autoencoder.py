9cb1ace3987bd602aa02c34f6a66e8c424e315c5
Ramtin Nouri <ramtin.nouri@outlook.de>
2022-11-08 15:42:38 +0100

    added ConvLSTM implementation

--- models/lstm_autoencoder.py
+++ models/lstm_autoencoder.py
@@ -5,10 +5,92 @@ import numpy as np
 from einops import rearrange
 from helper import *
 
+"""Implementation of ConvLSTMCell and ConvLSTM based on https://sladewinter.medium.com/video-frame-prediction-using-convlstm-network-in-pytorch-b5210a6ce582"""
+class ConvLSTMCell(LightningModule):
+    def __init__(self, in_channels, out_channels, 
+    kernel_size, padding, activation, frame_size):
+
+        super().__init__()  
+
+        if activation == "tanh":
+            self.activation = torch.tanh 
+        elif activation == "relu":
+            self.activation = torch.relu
+        
+        self.conv = nn.Conv2d(
+            in_channels=in_channels + out_channels, 
+            out_channels=4 * out_channels, 
+            kernel_size=kernel_size, 
+            padding=padding)           
+
+        # Initialize weights for Hadamard Products
+        self.W_ci = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+        self.W_co = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+        self.W_cf = nn.Parameter(torch.Tensor(out_channels, *frame_size))
+
+    def forward(self, X, H_prev, C_prev):
+
+        conv_output = self.conv(torch.cat([X, H_prev], dim=1))
+
+        i_conv, f_conv, C_conv, o_conv = torch.chunk(conv_output, chunks=4, dim=1)
+
+        input_gate = torch.sigmoid(i_conv + self.W_ci * C_prev )
+        forget_gate = torch.sigmoid(f_conv + self.W_cf * C_prev )
+
+        # Current Cell output
+        C = forget_gate*C_prev + input_gate * self.activation(C_conv)
+
+        output_gate = torch.sigmoid(o_conv + self.W_co * C )
+
+        # Current Hidden State
+        H = output_gate * self.activation(C)
+
+        return H, C
+    
+class ConvLSTM(nn.Module):
+
+    def __init__(self, in_channels, out_channels, 
+    kernel_size, padding, activation, frame_size, device):
+        super(ConvLSTM, self).__init__()
+        self.device = device
+
+        self.out_channels = out_channels
+
+        # We will unroll this over time steps
+        self.convLSTMcell = ConvLSTMCell(in_channels, out_channels, 
+        kernel_size, padding, activation, frame_size)
+
+    def forward(self, X):
+
+        # X is a frame sequence (batch_size, num_channels, seq_len, height, width)
+
+        # Get the dimensions
+        batch_size, _, seq_len, height, width = X.size()
+
+        # Initialize output
+        output = torch.zeros(batch_size, self.out_channels, seq_len, 
+        height, width, device=self.device)
+        
+        # Initialize Hidden State
+        H = torch.zeros(batch_size, self.out_channels, 
+        height, width, device=self.device)
+
+        # Initialize Cell Input
+        C = torch.zeros(batch_size,self.out_channels, 
+        height, width, device=self.device)
+
+        # Unroll over time steps
+        for time_step in range(seq_len):
+
+            H, C = self.convLSTMcell(X[:,:,time_step], H, C)
+
+            output[:,:,time_step] = H
+
+        return output
 
 class LstmEncoder(LightningModule):
     """Encoder of the LSTM model. 
-    Uses CNN to extract features from the input image, then feeds the features to the LSTM.
+    Uses ConvLSTM Cells to encode the input video.
 
     Args:
         config (dict): Dictionary containing the configuration of the model.
