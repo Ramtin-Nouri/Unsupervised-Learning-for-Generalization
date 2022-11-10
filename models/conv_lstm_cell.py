3b64e2e59252114b6dcd93c7f4f35a06812876cd
Ramtin Nouri <ramtin.nouri@outlook.de>
2022-11-10 10:40:53 +0100

    comments and formatting

--- models/conv_lstm_cell.py
+++ models/conv_lstm_cell.py
@@ -1,25 +1,28 @@
+"""
+    Convolutional LSTM Cell
+    Pretty much the same as: https://github.com/holmdk/Video-Prediction-using-PyTorch
+"""
 import torch.nn as nn
 import torch
 from pytorch_lightning import LightningModule
 
+
 class ConvLSTMCell(LightningModule):
+    """Convolutional LSTM cell.
+    See: https://arxiv.org/pdf/1506.04214v2.pdf
+
+    Args:
+        input_dim (int): Number of channels of the input tensor.
+        hidden_dim (int): Number of channels of the hidden state.
+        kernel_size (tuple): Size of the convolutional kernel.
+        bias (bool): Whether to use bias in the convolutional layers.
+
+    Attributes:
+        conv (nn.Conv2d): Convolutional layer.
+        hidden_dim (int): Number of channels of the hidden state.
+    """
 
     def __init__(self, input_dim, hidden_dim, kernel_size, bias):
-        """
-        Initialize ConvLSTM cell.
-
-        Parameters
-        ----------
-        input_dim: int
-            Number of channels of input tensor.
-        hidden_dim: int
-            Number of channels of hidden state.
-        kernel_size: (int, int)
-            Size of the convolutional kernel.
-        bias: bool
-            Whether or not to add the bias.
-        """
-
         super(ConvLSTMCell, self).__init__()
 
         self.input_dim = input_dim
@@ -36,12 +39,20 @@ class ConvLSTMCell(LightningModule):
                               bias=self.bias)
 
     def forward(self, input_tensor, cur_state):
+        """Forward pass of the ConvLSTM cell.
+
+        Args:
+            input_tensor (torch.Tensor): Input tensor of shape (batch_size, seq_len, input_dim, height, width).
+            cur_state (list): List containing the current hidden state and the current cell state.
+        """
         h_cur, c_cur = cur_state
 
-        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+        # concatenate along channel axis
+        combined = torch.cat([input_tensor, h_cur], dim=1)
 
         combined_conv = self.conv(combined)
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        cc_i, cc_f, cc_o, cc_g = torch.split(
+            combined_conv, self.hidden_dim, dim=1)
         i = torch.sigmoid(cc_i)
         f = torch.sigmoid(cc_f)
         o = torch.sigmoid(cc_o)
@@ -53,6 +64,15 @@ class ConvLSTMCell(LightningModule):
         return h_next, c_next
 
     def init_hidden(self, batch_size, image_size):
+        """Return zero vectors as initial hidden states.
+
+        Args:
+            batch_size (int): Batch size.
+            image_size (tuple): Size of the image.
+
+        Returns:
+            torch.Tensor: Initial hidden state.
+        """
         height, width = image_size
         return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
                 torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
